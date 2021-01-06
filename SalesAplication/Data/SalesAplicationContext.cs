388984b4d2702bf83063cb0084309b0b82aa0959
wagne <wagnergabrielbh@gmail.com>
2021-01-05 21:50:52 -0300

    CRUD Scaffonding

---
+++ SalesAplication/Data/SalesAplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesAplication.Models;
+
+namespace SalesAplication.Data
+{
+    public class SalesAplicationContext : DbContext
+    {
+        public SalesAplicationContext (DbContextOptions<SalesAplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesAplication.Models.Department> Department { get; set; }
+    }
+}
--- SalesAplication/Startup.cs
+++ SalesAplication/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesAplication.Data;
 
 namespace SalesAplication
 {
@@ -33,6 +35,9 @@ namespace SalesAplication
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SalesAplicationContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SalesAplicationContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
